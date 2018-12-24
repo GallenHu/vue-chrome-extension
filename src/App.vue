1ca46ccd4ae08f1d23069b7711abc8cdb0794cdb
GallenHu <huguiliang@zbj.com>
2018-12-24 16:12:43 +0800

    update: default style

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 500px;
+  max-height: 500px;
 }
 </style>
